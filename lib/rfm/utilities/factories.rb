e63f672a96adf19db62fbbc742fc339c82187fb5
Larry Sprock <larry@lucidbleu.com>
2010-02-24 17:59:39 -0800

    Added script to autoload when called from ScriptFactory since it isn't used that often.

--- lib/rfm/utilities/factories.rb
+++ lib/rfm/utilities/factories.rb
@@ -1,4 +1,3 @@
-autoload :Script, "rfm/utilities/script"
 # The classes in this module are used internally by Rfm and are not intended for outside
 # use.
 #
@@ -6,65 +5,69 @@ autoload :Script, "rfm/utilities/script"
 # Copyright:: Copyright (c) 2007 Six Fried Rice, LLC and Mufaddal Khumri
 # License::   See MIT-LICENSE for details
 
-module Factories
-  class DbFactory < Rfm::CaseInsensitiveHash
+module Rfm
+  autoload :Script, "rfm/utilities/script"
   
-    def initialize(server)
-      @server = server
-    end
+  module Factories
+    class DbFactory < Rfm::CaseInsensitiveHash
     
-    def [](dbname)
-      super or (self[dbname] = Rfm::Database.new(dbname, @server))
-    end
-    
-    def all
-      Rfm::ResultSet.new(@server, @server.do_action(@server.options[:account_name], @server.options[:password], '-dbnames', {}).body).each do |record|
-        name = record['database_name']
-        self[name] = Rfm::Database.new(name, @server) if self[name] == nil
+      def initialize(server)
+        @server = server
       end
-      self.values
-    end
-  
-  end
-  
-  class LayoutFactory < Rfm::CaseInsensitiveHash
-  
-    def initialize(server, database)
-      @server = server
-      @database = database
-    end
-    
-    def [](layout_name)
-      super or (self[layout_name] = Rfm::Layout.new(layout_name, @database))
-    end
-    
-    def all
-      Rfm::ResultSet.new(@server, @server.do_action(@server.options[:account_name], @server.options[:password], '-layoutnames', {"-db" => @database.name}).body).each do |record|
-        name = record['layout_name']
-        self[name] = Rfm::Layout.new(name, @database) if self[name] == nil
+      
+      def [](dbname)
+        super or (self[dbname] = Rfm::Database.new(dbname, @server))
       end
-      self.values
-    end
-  
-  end
-  
-  class ScriptFactory < Rfm::CaseInsensitiveHash
-    def initialize(server, database)
-      @server = server
-      @database = database
-    end
-    
-    def [](script_name)
-      super or (self[script_name] = Script.new(script_name, @database))
-    end
-    
-    def all
-      Rfm::ResultSet.new(@server, @server.do_action(@server.options[:account_name], @server.options[:password], '-scriptnames', {"-db" => @database.name}).body).each do |record|
-        name = record['script_name']
-        self[name] = Script.new(name, @database) if self[name] == nil
+      
+      def all
+        Rfm::ResultSet.new(@server, @server.do_action(@server.options[:account_name], @server.options[:password], '-dbnames', {}).body).each do |record|
+          name = record['database_name']
+          self[name] = Rfm::Database.new(name, @server) if self[name] == nil
+        end
+        self.values
       end
-      self.values
+    
     end
-  
+    
+    class LayoutFactory < Rfm::CaseInsensitiveHash
+    
+      def initialize(server, database)
+        @server = server
+        @database = database
+      end
+      
+      def [](layout_name)
+        super or (self[layout_name] = Rfm::Layout.new(layout_name, @database))
+      end
+      
+      def all
+        Rfm::ResultSet.new(@server, @server.do_action(@server.options[:account_name], @server.options[:password], '-layoutnames', {"-db" => @database.name}).body).each do |record|
+          name = record['layout_name']
+          self[name] = Rfm::Layout.new(name, @database) if self[name] == nil
+        end
+        self.values
+      end
+    
+    end
+    
+    class ScriptFactory < Rfm::CaseInsensitiveHash
+      def initialize(server, database)
+        @server = server
+        @database = database
+      end
+      
+      def [](script_name)
+        super or (self[script_name] = Rfm::Script.new(script_name, @database))
+      end
+      
+      def all
+        Rfm::ResultSet.new(@server, @server.do_action(@server.options[:account_name], @server.options[:password], '-scriptnames', {"-db" => @database.name}).body).each do |record|
+          name = record['script_name']
+          self[name] = Rfm::Script.new(name, @database) if self[name] == nil
+        end
+        self.values
+      end
+    
+    end
   end
 end
