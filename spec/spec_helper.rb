4333bc511daf210773196093dd2e616ab2aa040b
Larry Sprock <larry@lucidbleu.com>
2010-07-09 00:51:34 -0700

    Fixed some spec issues.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,3 +6,13 @@ require 'rfm'
 RSpec.configure do |c|
   c.mock_with :mocha
 end
+
+def rescue_from(&block)
+  exception = nil
+  begin
+    yield
+  rescue StandardError => e
+    exception = e
+  end
+  exception
+end
