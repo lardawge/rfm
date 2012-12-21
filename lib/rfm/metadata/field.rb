e94c2547a005030705b478264c7186880dad0cf1
Larry Sprock <larry@lucidbleu.com>
2012-12-21 15:10:31 -0800

    Cleanup trailing whitespace

--- lib/rfm/metadata/field.rb
+++ lib/rfm/metadata/field.rb
@@ -45,7 +45,7 @@ module Rfm
     #
     # * *control& is a FieldControl object representing the sytle and value list information associated
     #   with this field on the layout.
-    # 
+    #
     # Note: Since a field can sometimes appear on a layout more than once, +control+ may be an Array.
     # If you don't know ahead of time, you'll need to deal with this. One easy way is:
     #
@@ -56,11 +56,11 @@ module Rfm
     #
     # The code above makes sure the control is always an array. Typically, though, you'll know up front
     # if the control is an array or not, and you can code accordingly.
-    
+
     class Field
-      
+
       attr_reader :name, :result, :type, :max_repeats, :global
-      
+
       # Initializes a field object. You'll never need to do this. Instead, get your Field objects from
       # ResultSet::fields
       def initialize(field)
@@ -70,8 +70,8 @@ module Rfm
         @max_repeats = field['max-repeats']
         @global      = field['global']
       end
-    
-      # Coerces the text value from an +fmresultset+ document into proper Ruby types based on the 
+
+      # Coerces the text value from an +fmresultset+ document into proper Ruby types based on the
       # type of the field. You'll never need to do this: Rfm does it automatically for you when you
       # access field data through the Record object.
       def coerce(value, resultset)
@@ -85,9 +85,9 @@ module Rfm
         when "container" then URI.parse("#{resultset.server.scheme}://#{resultset.server.host_name}:#{resultset.server.port}#{value}")
         else nil
         end
-        
+
       end
-      
+
     end
   end
 end
